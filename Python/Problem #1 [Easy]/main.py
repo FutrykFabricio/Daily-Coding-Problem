a066b3443912e264a0f90a2151fd48481b8d94f4
Belseir <ffutryk52@gmail.com>
2021-06-23 20:29:33 -0300

    #1 Problem Solution

---
+++ Python/Problem #1 [Easy]/main.py
@@ -0,0 +1,15 @@
+n_list = [10, 15, 3, 7]
+k = 17
+
+def solve(n_list, k): 
+    for n in n_list:
+        aux = k
+        aux -= n
+
+        if aux in n_list:
+            print(f"{n} + {aux} = {k}")
+            return True
+
+    return False
+
+print(solve(n_list, k))
